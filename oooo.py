68eac1f50c1b4cd6cf44e3cf061573aebc1e2f03
student <student@itstep1.dp.ua>
2024-11-02 10:30:23 +0200

    1 created branch dev_Lesson1

--- oooo.py
+++ oooo.py
@@ -1,19 +1,18 @@
 print('lesson3')
 
 
-
 class StudySubject:
     name: str
     hours: int
     level: str
 
-    def __init__(self, name:str, hours: int, level:str):
+    def __init__(self, name: str, hours: int, level: str):
         self.name = name
         self.hours = hours
         self.level = level
 
     def __str__(self):
-       return f'{self.name} {self.level}, количество часов{self.hours}'
+        return f'{self.name} {self.level}, количество часов {self.hours}'
 
 
 class Student:
@@ -23,29 +22,19 @@ class Student:
     is_offline: bool
     study_subject: StudySubject
 
-    def __init__(self, second_name: str, first_name: str, age: int, study_subject: StudySubject, is_offline = True):
+    def __init__(self, second_name: str, first_name: str, age: int, study_subject: StudySubject, is_offline=True):
         self.first_name = first_name
         self.second_name = second_name
         self.age = age
         self.is_offline = is_offline
-        self.study_subject
-
+        self.study_subject = study_subject
 
     def __str__(self):
-
-        study_type: str
-        if self.is_offline:
-            study_type = 'offline'
-        else:
-             study_type = 'online'
-
-
-
-        study_info f'{self.first_name}{self.first_name}, возраст {self.age}'
-
+        study_type = 'offline' if self.is_offline else 'online'
+        student_info = f'{self.second_name} {self.first_name}, возраст {self.age}, тип обучения: {study_type}'
         return f'{student_info}\n{self.study_subject}'
 
-py_senior = StudySubject('Python', 18, 'Senior')
 
-ChernikovGerman = Student('Черников', "Макар", 15, py_senior)
+py_senior = StudySubject('Python', 18, 'Senior')
+ChernikovGerman = Student('Черников', "Герман", 15, py_senior)
 print(ChernikovGerman)
