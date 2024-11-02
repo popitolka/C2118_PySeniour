ace291482c77d40cec8f394bdd4d4d64efc99f94
student <student@itstep1.dp.ua>
2024-11-02 11:02:28 +0200

    1 created branch dev_Lesson1

--- oooo.py
+++ oooo.py
@@ -15,26 +15,44 @@ class StudySubject:
         return f'{self.name} {self.level}, количество часов {self.hours}'
 
 
+class Audience:
+    day: str
+    time_day: int
+    teacher: str
+
+    def __init__(self, day: str, time_day: int, teacher: str):
+        self.day = day
+        self.time_day = time_day
+        self.teacher = teacher
+
+    def __str__(self):
+        return f'День: {self.day}, время: {self.time_day} ч, преподаватель: {self.teacher}'
+
+
 class Student:
     first_name: str
     second_name: str
     age: int
     is_offline: bool
     study_subject: StudySubject
+    audience: Audience
 
-    def __init__(self, second_name: str, first_name: str, age: int, study_subject: StudySubject, is_offline=True):
+    def __init__(self, second_name: str, first_name: str, age: int, study_subject: StudySubject, audience: Audience, is_offline=True):
         self.first_name = first_name
         self.second_name = second_name
         self.age = age
         self.is_offline = is_offline
         self.study_subject = study_subject
+        self.audience = audience
 
     def __str__(self):
         study_type = 'offline' if self.is_offline else 'online'
         student_info = f'{self.second_name} {self.first_name}, возраст {self.age}, тип обучения: {study_type}'
-        return f'{student_info}\n{self.study_subject}'
+        return f'{student_info}\n{self.study_subject}\n{self.audience}'
 
 
+C2118 = Audience('Saturday', 9, 'Polishko Konstantin')
 py_senior = StudySubject('Python', 18, 'Senior')
-ChernikovGerman = Student('Черников', "Герман", 15, py_senior)
+ChernikovGerman = Student('Черников', "Герман", 15, py_senior, C2118)
+
 print(ChernikovGerman)
